cbd6c028a7ee9c483625043ffa1299e4f4de0d72
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-06 00:11:25 +0500

    Update Power.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-        int result;
+        int result=1;
         for (int i = 0; i < power; i++) {
             result *= numberToPrint;
         }
